d1fc8439c450a5a5fa2132e09803344b889171c2
Craig Watkinson <craig.watkinson@cambridgeappsolutions.com>
2014-07-30 23:06:28 +0100

    introspection

--- www/js/ble.js
+++ www/js/ble.js
@@ -36,7 +36,10 @@ function doIntro() {
     myInt=3;
     myStr="Hello";
     document.write("Introspection<br/>");
-    document.write("myInt: " + typeof(myInt));
-    document.write("myStr: " + typeof(myStr));
+    document.write("myInt: " + typeof(myInt) + "<br/>");
+    document.write("myStr: " + typeof(myStr) + "<br/>");
+    document.write("bluetoothle: " + typeof(bluetoothle) + "<br/>");
+    document.write("app: " + typeof(app) + "<br/>");
+    document.write("craig: " + typeof(craig) + "<br/>");
 }
           
